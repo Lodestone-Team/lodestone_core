6c38b4ef56dc8df43fcb9d2ecd1b43fd52e54ce1
Peter Jiang <2892084432j@gmail.com>
2022-10-01 13:54:29 -0400

    Add checks API

---
+++ src/handlers/checks.rs
@@ -0,0 +1,25 @@
+use crate::AppState;
+use axum::{extract::Path, Extension, Json};
+
+/// Check whether a port is in use
+/// Note: this function is not cheap
+pub async fn is_port_in_use(
+    Extension(state): Extension<AppState>,
+    Path(port): Path<u32>,
+) -> Json<bool> {
+    Json(state.port_allocator.lock().await.is_port_in_use(port))
+}
+
+/// Check whether a name is in use
+/// Note: this function is not cheap
+pub async fn is_name_in_use(
+    Extension(state): Extension<AppState>,
+    Path(name): Path<String>,
+) -> Json<bool> {
+    for (_, instance) in state.instances.lock().await.iter() {
+        if instance.lock().await.name() == name {
+            return Json(true);
+        }
+    }
+    Json(false)
+}
