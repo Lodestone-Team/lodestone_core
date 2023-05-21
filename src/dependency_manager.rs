84daabb9d8208afb36bffb1f918987d0795acfef
Hanmin Kim <hanminkim123@gmail.com>
2023-05-20 18:21:43 -0700

    rework dependency_manager to use Result instead of panicking

--- src/dependency_manager.rs
+++ src/dependency_manager.rs
@@ -1,68 +1,80 @@
 extern crate serde_json;
 
-use std::fs::File;
 use std::collections::HashMap;
-use std::io::ErrorKind;
+use std::fs::File;
+use std::io;
+use std::io::{ErrorKind};
 
 const STATE_FILE_PATH: &str = "./bin/dependencies.json";
 
+pub enum DependencyManagerError {
+    IoError(io::Error),
+    SerdeError(serde_json::Error),
+    NotFoundError,
+}
+
 pub struct DependencyManager {
-    registered_paths: HashMap<String, String>,
+    registered_paths: Option<HashMap<String, String>>,
 }
 
 impl DependencyManager {
     fn new() -> DependencyManager {
-        let mut d = DependencyManager {
-            registered_paths: HashMap::new(),
-        };
-        d.load();
-        d
-    }
-
-    fn save(&self) {
-        let file = File::create(STATE_FILE_PATH);
-        match file {
-            Ok(file) => {
-                serde_json::to_writer(file, &self.registered_paths).unwrap();
-            }
-            Err(_) => {
-                println!("Failed to save dependencies");
-            }
+        DependencyManager {
+            registered_paths: None,
         }
     }
 
-    fn load(&mut self) {
+    fn save(&self) -> Result<(), DependencyManagerError> {
+        let file = File::create(STATE_FILE_PATH);
+        return match file {
+            Ok(file) => match serde_json::to_writer(file, &self.registered_paths) {
+                Ok(_) => Ok(()),
+                Err(e) => Err(SaveError::SerdeError(e))
+            },
+            Err(e) => Err(SaveError::IoError(e))
+        }
+    }
+
+    fn load(&mut self) -> Result<(), DependencyManagerError> {
+        if let Some(_) = self.registered_paths {
+            return Ok(())
+        }
+
         let file = File::open(STATE_FILE_PATH);
         match file {
             Ok(file) => {
                 let dependencies: HashMap<String, String> = serde_json::from_reader(file).unwrap();
-                self.registered_paths = dependencies;
+                self.registered_paths = Option::from(dependencies);
+                Ok(())
             }
-            Err(error) => {
-                match error.kind() {
-                    ErrorKind::NotFound => {
-                        match File::create(STATE_FILE_PATH) {
-                            Ok(_) => {},
-                            Err(e) => panic!("Problem creating the file: {:?}", e),
-                        }
-                    }
-                    other_error => {
-                        panic!("Problem opening the file: {:?}", other_error);
-                    }
+            Err(error) => return match error.kind() {
+                ErrorKind::NotFound => match File::create(STATE_FILE_PATH) {
+                    Ok(_) => Ok(()),
+                    Err(e) => Err(DependencyManagerError(e)),
+                },
+                other_error => {
+                    Err(DependencyManagerError(io::Error::from(other_error)))
                 }
             }
         }
     }
 
-    pub fn register(&mut self, name: String, path: String) {
-        self.registered_paths.insert(name, path);
-        self.save();
+    pub fn register(&mut self, name: String, path: String) -> Result<(), DependencyManagerError> {
+        self.load()?;
+
+        match &self.registered_paths {
+            Some(mut hashMap) => hashMap.insert(name, path),
+            None => ()
+        }
+        self.save()
     }
 
-    pub fn get(&self, name: String) -> &String {
+    pub fn get(&mut self, name: String) -> Result<&String, E> {
+        self.load()?;
+
         match self.registered_paths.get((&name).as_ref()) {
-            Some(path) => path,
-            None => panic!("Dependency {} not found", name)
+            Some(path) => Ok(path),
+            None => Err(DependencyManagerError::NotFoundError),
         }
     }
 }
